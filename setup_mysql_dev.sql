616979760db312edc35cdae9b4bcd166f22962be
AndrewMiranda <40378450+AndrewMiranda@users.noreply.github.com>
2021-03-29 13:34:35 -0500

    Update

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,9 +1,8 @@
 -- Creates a DataBase
-CREATE DATABASE IF NOT EXISTS hbtn_dev_db;
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 -- Create a New user
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 -- Run database
-USE hbnb_dev_db;
-GRANT ALL PRIVILEGES ON `hbnb_dev_db` .* TO 'hbnb_dev'@'localhost';
-GRANT SELECT ON `performance_schema` .* TO 'hbnb_dev'@'localhost';
-FLUSH PRIVILEGES;
+GRANT USAGE ON *.* TO 'hbnb_dev'@'localhost';
+GRANT ALL ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
